f7316bf3e117a7e0ee89f00bd8715d81ac31e3d2
Ryan Vilbrandt <ryan.v@zonarsystems.com>
2017-03-20 09:40:51 -0700

    Update wolfram_pixel to use PIL instead of TKinter
    Also, added some pretty colors

--- wolfram_pixels.py
+++ wolfram_pixels.py
@@ -1,7 +1,12 @@
-import Tkinter, random
+from random import randint
 
-W,B,R=(255,255,255),(0,0,0),(255,0,0)
-BITS = [W,R]    # empty, filled pixel color
+from PIL import Image
+
+# PALETTE is a colorful option that uses the location of the pixel on the image to determine its color.as
+# x will be R, y will be G, and the actual value of PALETTE will be B.
+
+W, B, R, PALETTE = (255, 255, 255), (0, 0, 0), (255, 0, 0), 100
+BITS = [W, PALETTE]   # empty, filled pixel color
 LEFT_EDGE = 0   # Spoof left edge of each row as empty or filled?
 RIGHT_EDGE = 0  # Spoof right edge of each row as empty or filled?
 RANDOMIZE_FIRST_LINE = False
@@ -9,12 +14,12 @@ RANDOMIZE_RULES = False
 
 class App:
 
-    width = 20
-    height = 10
+    width = 4095
+    height = 2048
 
 ##    rules = [1, 0, 0, 1, 1, 0, 1, 0] #frost
 ##    rules = [1, 0, 0, 1, 1, 1, 0, 0] #mountains
-##    rules = [0, 1, 0, 0, 1, 0, 0, 0] #sierpinski
+    # rules = [0, 1, 0, 0, 1, 0, 0, 0] #sierpinski
     rules = [0, 1, 1, 0, 1, 0, 0, 0] #sierpinski2
 ##    rules = [1, 0, 1, 0, 0, 1, 0, 1] #raindrops
 ##    rules = [1, 1, 0, 1, 0, 0, 0, 0] #wrinkles
@@ -29,27 +34,25 @@ class App:
 ##             1,     #111
 ##             ]
     
-    def __init__(self, t):
-        self.i = Tkinter.PhotoImage(width=self.width,height=self.height)
+    def __init__(self):
         if RANDOMIZE_RULES:
             self.RandomizeRules()
-        print self.rules
-        print "Building array..."
+        print(self.rules)
+        print("Building array...")
         bitmap = self.BuildBitmap()
-        print "Converting to colors..."
-        bitmap = [[str(BITS[b]) for b in line]
-                  for line in bitmap]
+        print("Converting to colors...")
+        bitmap = [[BITS[b] for b in line] for line in bitmap]
         # print bitmap
-        print "Painting bitmap..."
+        print("Painting bitmap...")
         self.PaintBitmap(bitmap)
 
     def BuildBitmap(self):
         bitmap = []
         b = []
         # Setup first line
-        for i in xrange(self.width):
+        for i in range(self.width):
             if RANDOMIZE_FIRST_LINE:
-                b.append(random.randint(0,1))
+                b.append(randint(0, 1))
             else:
                 if i == self.width // 2:
                     b.append(1)
@@ -58,22 +61,18 @@ class App:
         bitmap.append(b)
 
         # Main
-        for y in xrange(1, self.height):
+        for y in range(1, self.height):
+            if y % 100 == 0:
+                print("{:.0%} ({}/{} rows)".format(y / float(self.height), y, self.height))
             b = []
-            # For bits at the beginning of lines,
-            # fake an upper left bit as empty
-            # if LEFT_EDGE is 0
+            # For bits at the beginning of lines, fake an upper left bit as empty if LEFT_EDGE is 0
             i = (y - 1) * self.width
             b.append(self.GetRule(LEFT_EDGE,
                                   bitmap[-1][0],
                                   bitmap[-1][1]))
-            for i in xrange(1,self.width-1):
-##                i = (y-1)*self.width + x
-##                print y,x,i
-                b.append(self.GetRule(bitmap[-1][i-1], bitmap[-1][i], bitmap[-1][i+1]))
-            # For bits at the end of lines,
-            # fake an upper right bit as empty
-            # if RIGHT_EDGE is 0
+            for i in range(1, self.width-1):
+                b.append(self.GetRule(bitmap[-1][i - 1], bitmap[-1][i], bitmap[-1][i + 1]))
+            # For bits at the end of lines, fake an upper right bit as empty if RIGHT_EDGE is 0
             i += 1
             b.append(self.GetRule(bitmap[-1][-2],
                                   bitmap[-1][-1],
@@ -86,30 +85,21 @@ class App:
         return self.rules[(a << 2) + (b << 1) + c]
 
     def RandomizeRules(self):
-        self.rules = [random.randint(0, 1) for i in xrange(8)]
+        self.rules = [randint(0, 1) for _ in range(8)]
 
     def PaintBitmap(self, bitmap):
-        row = 0
-        col = 0
-        for bit in bitmap:
-            # print bit
-            # try:
-            #     self.i.put("{" + " ".join(bit) + "}")
-            # except Exception as e:
-            #     print e
-            #     print repr(bit)
-            #     raise
-            self.i.put('#%02x%02x%02x' % tuple(bit), (col, row))
-            col += 1
-            if col == self.width:
-                if row % 100 == 0:
-                    print "{0:.0%} ({1}/{2} rows)".format(row / float(self.height), row, self.height)
-                row += 1
-                col = 0
-        c = Tkinter.Canvas(t, width=self.width, height=self.height)
-        c.pack()
-        c.create_image(0, 0, image = self.i, anchor=Tkinter.NW)
+        img = Image.new('RGB', (self.width, self.height), "white")
+        pixels = img.load()
+        for y in range(self.height):
+            # Print row
+            if y % 100 == 0:
+                print("{:.0%} ({}/{} rows)".format(y / float(self.height), y, self.height))
+            # Set pixels
+            for x in range(self.width):
+                if bitmap[y][x] == PALETTE:
+                    pixels[x, y] = (int((x / float(self.width)) * 256), int((y / float(self.height)) * 256), PALETTE)
+                else:
+                    pixels[x, y] = bitmap[y][x]
+        img.show()
 
-t = Tkinter.Tk()
-a = App(t)    
-t.mainloop()
+a = App()
