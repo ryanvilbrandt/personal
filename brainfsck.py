5486592941587f971ff0b1132627dcbd4093ccb6
Ryan Vilbrandt <ryan.v@zonarsystems.com>
2017-06-13 10:28:32 -0700

    Formatting fixes

--- brainfsck.py
+++ brainfsck.py
@@ -1,23 +1,23 @@
 import sys, time
 
 MEM_ARRAY_SIZE = 16
-MEM_BITS = 8
+MEM_BITS = 64
 VERBOSE = False
 PRINT_CHAR = False
 
 def print_code(in_str, codeptr, dataptr, mem, iters):
-    print in_str
-    print " "*codeptr+"^"
-    print "{0}: {1} ({2} iters)".format(dataptr,mem,iters)
+    print(in_str)
+    print(" " * codeptr + "^")
+    print("{0}: {1} ({2} iters)".format(dataptr, mem, iters))
 
 def main(in_str):
-    mem = [0]*MEM_ARRAY_SIZE
+    mem = [0] * MEM_ARRAY_SIZE
     codeptr = 0
     dataptr = 0
     levelarray = []
     iters = 0
     bits_mod = 1 << MEM_BITS
-    while (codeptr < len(in_str)):
+    while codeptr < len(in_str):
         iters += 1
         if in_str[codeptr] == ">":
             # Mod allows for circular memory buffer
@@ -35,9 +35,9 @@ def main(in_str):
                 sys.stdout.write(str(mem[dataptr]))
         elif in_str[codeptr] == ",":
             req = None
-            print "\nGive me one byte of input, use quotes for a char: "
+            print("\nGive me one byte of input, use quotes for a char: ")
             while (req == None):
-                req = raw_input()
+                req = input()
                 try:
                     req = int(req) & 0xFF
                 except ValueError:
@@ -45,10 +45,10 @@ def main(in_str):
                         if req[0] == '"' or "'":
                             req = ord(req[1])
                         else:
-                            print "Invalid input"
+                            print("Invalid input")
                             req = None
                     except IndexError:
-                        print "Invalid input"
+                        print("Invalid input")
                         req = None
             mem[dataptr] = req
         elif in_str[codeptr] == "[":
@@ -57,11 +57,11 @@ def main(in_str):
                 err_ptr = codeptr
                 i = 0
                 # Use -1 because of oddness in code
-                while (i > -1):
+                while i > -1:
                     codeptr += 1
                     # If the open bracket doesn't have a matching ending bracket, error out
                     if codeptr >= len(in_str):
-                        print "\nEOL reached when scanning for ending bracket; unmatched [ at {0}".format(err_ptr)
+                        print("\nEOL reached when scanning for ending bracket; unmatched [ at {}".format(err_ptr))
                         codeptr = err_ptr
                         print_code(in_str, codeptr, dataptr, mem, iters)
                         return
@@ -81,21 +81,23 @@ def main(in_str):
             # Else, jump back to matching [
             else:
                 if len(levelarray) == 0:
-                    print "\nFound a ] without a matching [ at {0}".format(codeptr)
+                    print("\nFound a ] without a matching [ at {0}".format(codeptr))
                     print_code(in_str, codeptr, dataptr, mem, iters)
                     return
                 codeptr = levelarray[-1]
         if VERBOSE:
-            print ""
+            print("")
             print_code(in_str, codeptr, dataptr, mem, iters)
             time.sleep(0.1)
         codeptr += 1
 
+    print("\n\n{}: {} ({} iters)".format(dataptr, mem, iters))
+
 if __name__ == "__main__":
-    in_str = "Addition ,>,[-<+>]<."
+    # in_str = "Addition ,>,[-<+>]<."
     # in_str = "Multiplication ,>,<[>[>+>+<<-]>[<+>-]<<-]>>>."
-##    in_str = "Factorial ,[->+>+<<]>>->>+<<<[>[<[->[-<<+>>>+<]>[-<+>]<<]<[->+<]>>-]<.>>>-]>>>[.-]"
-    ##in_str = "++++[.-]"
+    in_str = "Factorial ,[->+>+<<]>>->>+<<<[>[<[->[-<<+>>>+<]>[-<+>]<<]<[->+<]>>-]<.>>>-]>>>[.-]"
+    #in_str = "++++[.-]"
 
     ##in_str = ""
     ##i = raw_input("Input brainfsck code now, type 'run' to end: ")
