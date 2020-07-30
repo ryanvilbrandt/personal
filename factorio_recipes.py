3e6f1d37350262953b833bc641c5da68f87a7dc6
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2020-07-29 18:19:03 -0700

    Update factorio_recipes.py

--- factorio_recipes.py
+++ factorio_recipes.py
@@ -19,16 +19,26 @@ class Item:
         for item, n in self.recipe.items():
             item.analyze(self.num_per_second * n)
 
-
-
-
+# Smelted goods
+STONE_BRICK = Item("Stone brick")
 IRON_PLATE = Item("Iron plate")
 COPPER_PLATE = Item("Copper plate")
+
+# Processed goods
 IRON_STICK = Item("Iron stick", 0.5, 2, {IRON_PLATE: 1})
 IRON_GEAR = Item("Iron gear wheel", 0.5, 1, {IRON_PLATE: 2})
 COPPER_CABLE = Item("Copper cable", 0.5, 2, {COPPER_PLATE: 1})
+STONE_TABLET = Item("Stone tablet", 0.5, 4, {STONE_BRICK: 1})
+
+# Manufactured goods
+ELECTRONIC_CIRCUIT = Item("Electronic circuit", 0.5, 1, {COPPER_CABLE: 3, STONE_TABLET: 1})
 MOTOR = Item("Motor", 0.6, 1, {IRON_GEAR: 1, IRON_PLATE: 1})
-ELECTRIC_MOTOR = Item("Electric Motor", 0.8, 1, {MOTOR: 1, COPPER_CABLE: 6})
+ELECTRIC_MOTOR = Item("Electric motor", 0.8, 1, {MOTOR: 1, COPPER_CABLE: 6})
+
+# Factory equipment
+BURNER_INSERTER = Item("Burner inserter", 0.5, 1, {IRON_STICK: 2, MOTOR: 1})
+ELECTRIC_INSERTER = Item("Inserter", 0.5, 1, {ELECTRIC_MOTOR: 1, BURNER_INSERTER: 1})
+FAST_INSERTER = Item("Fast inserter", 0.5, 1, {ELECTRONIC_CIRCUIT: 2, ELECTRIC_INSERTER: 1, IRON_PLATE: 2})
 
 
 ELECTRIC_MOTOR.analyze(0.8)
