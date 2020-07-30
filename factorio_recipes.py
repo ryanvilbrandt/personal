1c10c748d934b1d11185e3f7a12b4952251942bd
ryanvilbrandt <ryan.v@zonarsystems.com>
2020-07-29 17:36:50 -0700

    Update to factorio_recipes

--- factorio_recipes.py
+++ factorio_recipes.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from math import ceil
 
 
 class Item:
@@ -7,14 +8,16 @@ class Item:
         self.name = name
         self.crafting_time = crafting_time
         self.num_created = num_created
+        self.num_per_second = 0 if crafting_time == 0 else num_created / crafting_time
         self.recipe = recipe
 
-    def analyze(self, num_factories):
-        if self.crafting_time == 0:
-            return {self.name: 1}
-        ingredients = defaultdict(int)
-        for parent_item, parent_n in self.recipe.items():
-            for item, n in parent_item.analyze():
+    def analyze(self, num_per_second=1.0):
+        if self.num_per_second == 0:
+            print("{}/s {} needed".format(num_per_second, self.name))
+            return
+        print("{}/s {} needed ({} factories)".format(num_per_second, self.name, ceil(num_per_second / self.num_per_second)))
+        for item, n in self.recipe.items():
+            item.analyze(self.num_per_second * n)
 
 
 
@@ -25,3 +28,7 @@ IRON_STICK = Item("Iron stick", 0.5, 2, {IRON_PLATE: 1})
 IRON_GEAR = Item("Iron gear wheel", 0.5, 1, {IRON_PLATE: 2})
 COPPER_CABLE = Item("Copper cable", 0.5, 2, {COPPER_PLATE: 1})
 MOTOR = Item("Motor", 0.6, 1, {IRON_GEAR: 1, IRON_PLATE: 1})
+ELECTRIC_MOTOR = Item("Electric Motor", 0.8, 1, {MOTOR: 1, COPPER_CABLE: 6})
+
+
+ELECTRIC_MOTOR.analyze(0.8)
