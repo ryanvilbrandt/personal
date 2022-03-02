7d8e6ed97e3edf7e33dcd473867e6ffdee6493f7
Ryan Vilbrandt <ryan.v@zonarsystems.com>
2022-03-01 18:19:37 -0800

    Update youtube-dl.py

--- youtube-dl.py
+++ youtube-dl.py
@@ -8,7 +8,16 @@ ydl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    "outtmpl": "%(playlist_index)s - %(title)s.%(ext)s"
+    # "outtmpl": "%(playlist_index)s - %(title)s.%(ext)s"
+    "outtmpl": "%(title)s.%(ext)s"
 }
+
+# Download video with subtitle
+# ydl_opts = {
+#     "writesubtitles": True,
+#     # "skip_download": True,
+# }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(["https://www.youtube.com/playlist?list=PLvNp0Boas720sJW-iF7HFdHQqm03VKIj9"])
+    ydl.download([
+        "https://www.youtube.com/watch?v=H_zWESQPKu8"
+    ])
